4680764d596749ca1d95e691c0fb7b6aca8058af
Tommy Byrd <tcbyrd@github.com>
2017-02-22 17:17:11 -0500

    Making it work, I PROMISE!

--- app/controllers/papers.js
+++ app/controllers/papers.js
@@ -1,52 +1,67 @@
 var express = require('express')
 var router = express.Router()
 const base = require('airtable').base('appf6aPxwZ2Jn78Os');
+let papers = [];
+
+function getPapers(filterOpts) {
+  return new Promise(function(resolve, reject) {
+  let newPapers = []
+  base('Imported Table').select(filterOpts).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('Submission ID'));
+        var id = record.get('Submission ID')
+        var title = record.get('Title')
+        var abstract = record.get('Abstract')
+        var order = record.get('Order in Session')
+        var day = record.get('Day')
+        var startTime = record.get('Time Start')
+        var endTime = record.get('Time End')
+        var sessionType = record.get('Session Type')
+        var trackTitle = record.get('Track title')
+        var firstName1 = record.get('First name (Author 1)')
+        var lastName1 = record.get('Last name (Author 1)')
+        var firstName2 = record.get('First name (Author 2)')
+        var lastName2 = record.get('Last name (Author 2)')
+        var abstract = record.get('Abstract')
+        var newPaper = {
+          id: id,
+          title: title,
+          abstract: abstract,
+          order: order,
+          day: day,
+          startTime: startTime,
+          endTime: endTime,
+          sessionType: sessionType,
+          trackTitle: trackTitle,
+          firstName1: firstName1,
+          lastName1: lastName1,
+          firstName2: firstName2,
+          lastName2: lastName2
+        }
+        // console.log(newPaper);
+        newPapers.push(newPaper)
+    });
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+    }, function done(err) {
+        if (err) { console.error(err); return; }
+        papers = newPapers
+        resolve(papers)
+    });
+  });
+}
 
 router.get('/papers/abstracts', function (req, res, next) {
   var filterOpts = {
     // Selecting the first 100 records in Abstracts view:
-    maxRecords: 100,
+    maxRecords: 400,
     view: "Abstracts"
   }
-  base('Imported Table').select(filterOpts).eachPage(function page(records, fetchNextPage) {
-      // This function (`page`) will get called for each page of records.
-      var papers = []
-      records.forEach(function(record) {
-          console.log('Retrieved', record.get('Submission ID'));
-          var id = record.get('Submission ID')
-          var title = record.get('Title')
-          var abstract = record.get('Abstract')
-          var order = record.get('Order in Session')
-          var day = record.get('Day')
-          var sessionType = record.get('Session Type')
-          var trackTitle = record.get('Track title')
-          var firstName1 = record.get('First name (Author 1)')
-          var lastName1 = record.get('Last name (Author 1)')
-          var firstName2 = record.get('First name (Author 2)')
-          var lastName2 = record.get('Last name (Author 2)')
-          var abstract = record.get('Abstract')
-          var newPaper = {
-            id: id,
-            title: title,
-            abstract: abstract,
-            trackTitle: trackTitle,
-            firstName1: firstName1,
-            lastName1: lastName1,
-            firstName2: firstName2,
-            lastName2: lastName2,
-          }
-          // console.log(newPaper);
-          papers.push(newPaper)
-      });
-
-      // To fetch the next page of records, call `fetchNextPage`.
-      // If there are more records, `page` will get called again.
-      // If there are no more records, `done` will get called.
-      res.render('abstracts', {title: 'abstracts', papers})
-
-  }, function done(err) {
-      if (err) { console.error(err); res.send('error fetching data from AirTable'); }
-  });
+  getPapers(filterOpts)
+    .then(papers => res.render('abstracts', {title: 'abstracts', papers}));
 });
 
 router.get('/papers/:day', function (req, res, next){
@@ -80,7 +95,6 @@ router.get('/papers/:day', function (req, res, next){
     var filterOpts = {
       // Selecting the first 25 records in the track defined in 'view':
       maxRecords: 400,
-      pageSize: 100,
       view: view,
       filterByFormula: filterDate
     }
@@ -92,51 +106,8 @@ router.get('/papers/:day', function (req, res, next){
     }
   }
   console.log('Formula: ', filterOpts.filterByFormula);
-  base('Imported Table').select(filterOpts).eachPage(function page(records, fetchNextPage) {
-      // This function (`page`) will get called for each page of records.
-      var papers = []
-      records.forEach(function(record) {
-          console.log('Retrieved', record.get('Submission ID'));
-          var id = record.get('Submission ID')
-          var title = record.get('Title')
-          var abstract = record.get('Abstract')
-          var order = record.get('Order in Session')
-          var day = record.get('Day')
-          var startTime = record.get('Time Start')
-          var endTime = record.get('Time End')
-          var sessionType = record.get('Session Type')
-          var trackTitle = record.get('Track title')
-          var firstName1 = record.get('First name (Author 1)')
-          var lastName1 = record.get('Last name (Author 1)')
-          var firstName2 = record.get('First name (Author 2)')
-          var lastName2 = record.get('Last name (Author 2)')
-          var newPaper = {
-            id: id,
-            title: title,
-            abstract: abstract,
-            order: order,
-            day: day,
-            startTime: startTime,
-            endTime: endTime,
-            sessionType: sessionType,
-            trackTitle: trackTitle,
-            firstName1: firstName1,
-            lastName1: lastName1,
-            firstName2: firstName2,
-            lastName2: lastName2
-          }
-          // console.log(newPaper);
-          papers.push(newPaper)
-      });
-      // To fetch the next page of records, call `fetchNextPage`.
-      // If there are more records, `page` will get called again.
-      // If there are no more records, `done` will get called.
-      res.render('papers', {title: 'Papers', day: day, papers})
-
-  }, function done(err) {
-      if (err) { console.error(err); res.send('error fetching data from AirTable'); }
-  });
-
+  getPapers(filterOpts)
+    .then(papers => res.render('papers', {title: 'Papers', day: day, papers}))
 });
 
 // router.get('/papers', function (req, res, next) {
