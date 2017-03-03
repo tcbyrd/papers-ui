c78c73fce66aa8bc5955a768d9dfe04ebc2d6053
Tommy Byrd <tcbyrd@github.com>
2017-03-03 18:42:09 -0500

    Made a 2 column printable view and updated logos

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,9 @@
-var getNextPage = function () {
-  
+var switchColumn = function () {
+  var content = document.getElementsByClassName('content-col-1')[0]
+  if (content) {
+    content.className = 'content-col-2'
+  }
+  else {
+    document.getElementsByClassName('content-col-2')[0].className = 'content-col-1'
+  }
 }
